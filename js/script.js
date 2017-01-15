dd392a58be5cb334fd4ceb326cb30a66c862a3a5
yiskang <m9805508@mail.ntust.edu.tw>
2017-01-15 22:05:51 +0800

    Move blog to new location and make this page as my resume.

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var tables = document.querySelectorAll('.post-content > table')
-for (var i = 0; i < tables.length; i++) {
-  tables[i].setAttribute('class','pure-table pure-table-bordered')
-}
-
-var imgs = document.querySelectorAll('.post-content img')
-for (var i = 0; i < imgs.length; i++) {
-  imgs[i].setAttribute('class','pure-img')
-}
